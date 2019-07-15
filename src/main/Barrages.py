4ce0fa6a19912efe19d84628f5aa67d8ccec94fe
Pachurii <ALPachurii@protonmail.com>
2019-07-16 06:07:24 +0800

    feat(Barrages): Implement method to calculate barrage animation time

--- src/main/Barrages.py
+++ src/main/Barrages.py
@@ -5,6 +5,7 @@ class Barrage:
     """
     Barrage class describes in what pattern are the projectiles (bullets) created
     """
+
     def __init__(self, barrageData: Dict):
         self.id = barrageData["id"]
         self.offsetZ = barrageData["offset_z"]
@@ -15,6 +16,7 @@ class Barrage:
         self.deltaAngle = barrageData["delta_angle"]
         self.delayCast = barrageData["first_delay"]
         self.primalDelay = barrageData["delay"]
+        self.deltaPrimalDelay = barrageData["delta_delay"]
         self.primalRepeat = barrageData["primal_repeat"]
         self.seniorDelay = barrageData["senior_delay"]
         self.seniorRepeat = barrageData["senior_repeat"]
@@ -25,6 +27,16 @@ class Barrage:
         """
         Calculates the total projectiles created
 
-        :return: integer the projectiles count
+        :return: integer, the projectiles count
         """
         return (self.seniorRepeat + 1) * (self.primalRepeat + 1)
+
+    def getAnimationTime(self) -> float:
+        """
+        Calculates the firing animation time (from barrage start to the last wave being fired)
+
+        :return: float, the total animation time
+        """
+        return self.delayCast + (self.primalDelay * self.primalRepeat + max(0, 1 / 2 * (self.primalRepeat - 1) * (
+                self.primalRepeat - 2) * self.deltaPrimalDelay)) * (
+                       self.seniorRepeat + 1) + self.seniorDelay * self.seniorRepeat
